74fe84e91feca19bde30cd0157b8d3b9165febe9
Dan Fornika <dfornika@gmail.com>
2022-01-05 17:19:19 -0800

    Rename to sync-dirs.py and restore support for local transfers

---
+++ sync-dirs.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+
+import argparse
+import itertools
+import json
+import logging
+import pwd
+import socket
+import subprocess
+import sys
+import time
+import multiprocessing
+import os     
+
+from datetime import datetime
+
+import paramiko
+
+
+def sync(transfer_dir, src, dest):
+    logging.info(os.path.join(dest, transfer_dir))
+    transfer_start_time = datetime.now().astimezone().isoformat()
+
+    subprocess.call(["rsync", "-aq", os.path.join(src, transfer_dir), dest])
+    transfer_complete_time = datetime.now().astimezone().isoformat()
+    local_hostname = socket.gethostname()
+    if ':' in src:
+        src_hostname, src_dir = src.split(':') 
+        src = ':'.join([src_hostname, os.path.join(src, transfer_dir)])
+    else:
+        src = ':'.join([local_hostname, os.path.abspath(os.path.join(src, transfer_dir))])
+
+    transfer_complete = {
+        "operation": "data_migration",
+        "src": src,
+        "dest": ':'.join([local_hostname, os.path.abspath(os.path.join(dest, transfer_dir))]),
+        "timestamp_transfer_start": transfer_start_time,
+        "timestamp_transfer_complete": transfer_complete_time,
+    }
+
+    with open(os.path.join(dest, transfer_dir, 'transfer_complete.json'), 'w') as f:
+        json.dump(transfer_complete, f, indent=2)
+        f.write('\n')
+
+
+def list_dirs_remote(src):
+    src_hostname = src.split(':')[0]
+    src_dir = src.split(':')[1]
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    username = pwd.getpwuid(os.getuid())[0]
+    key_path = os.path.expanduser('~/.ssh/id_rsa')
+    client.connect(hostname=src_hostname, username=username, key_filename=os.path.expanduser('~/.ssh/id_rsa') )
+    command = ' '.join(['ls', '-1', src_dir])
+    stdin , stdout, stderr = client.exec_command(command)
+    transfer_dirs = list(filter(lambda x: x != "", stdout.read().decode('UTF-8').split('\n')))
+
+    return transfer_dirs
+
+
+def list_dirs_local(src):
+    transfer_dirs = []
+    for d in os.listdir(src):
+        if os.path.isdir(os.path.join(src, d)):
+            transfer_dirs.append(d)
+
+    return transfer_dirs
+
+
+def main(args):
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s :: %(levelname)s] %(message)s', datefmt="%Y-%m-%dT%H:%M:%S%z")
+
+    transfer_dirs = []
+    
+    if ':' in args.src:
+        transfer_dirs = list_dirs_remote(args.src)
+    else:
+        transfer_dirs = list_dirs_local(args.src)
+
+
+    with multiprocessing.Pool(processes=args.processes) as pool:
+        transfers = list(zip(transfer_dirs, itertools.cycle([args.src]), itertools.cycle([args.dest])))
+        pool.starmap(sync, transfers)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--processes', type=int, default=4, help="Number of simultaneous transfers")
+    parser.add_argument('-s', '--src', help="Source directory")
+    parser.add_argument('-d', '--dest', help="Destination directory")
+    args = parser.parse_args()
+    
+    main(args)
+
